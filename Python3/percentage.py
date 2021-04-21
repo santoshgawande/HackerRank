686dbd2802fb30017e6bbbb2a7e3feb8d1b9cc43
Santosh Gawande <gawandesantos@gmail.com>
2021-04-21 12:59:58 +0530

    add HackerRank Py3 solutions

---
+++ Python3/percentage.py
@@ -0,0 +1,21 @@
+# Input (stdin)
+#     3
+#     Krishna 67 68 69
+#     Arjun 70 98 63
+#     Malika 52 56 60
+#     Malika
+
+# Your Output (stdout)
+#     56.00
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    s = (sum(student_marks[query_name])/len(scores))
+    # format 2 decimal spaces
+    print(format(s,".2f"))
